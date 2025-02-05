fef1ed1f4d1a5cbf6cbe8911ea71a7d86c6d612c
Schmarni <marnistromer@gmail.com>
2025-02-05 20:50:56 +0100

    add more wgpu features
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- crates/bevy_openxr/src/openxr/graphics/vulkan.rs
+++ crates/bevy_openxr/src/openxr/graphics/vulkan.rs
@@ -195,7 +195,10 @@ unsafe impl GraphicsExt for openxr::Vulkan {
             | wgpu::Features::MULTI_DRAW_INDIRECT_COUNT
             | wgpu::Features::MULTI_DRAW_INDIRECT
             | wgpu::Features::TEXTURE_FORMAT_16BIT_NORM
-            | wgpu::Features::CLEAR_TEXTURE;
+            | wgpu::Features::CLEAR_TEXTURE
+            | wgpu::Features::MAPPABLE_PRIMARY_BUFFERS
+            | wgpu::Features::PUSH_CONSTANTS
+            | wgpu::Features::POLYGON_MODE_LINE;
 
         let Some(wgpu_exposed_adapter) = wgpu_vk_instance.expose_adapter(vk_physical_device) else {
             error!("WGPU failed to provide an adapter");
